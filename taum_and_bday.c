e348fb7def0b7870820f3a89dc8fcdb47481ded4
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-25 23:37:41 +0600

    hackerrank problem

--- taum_and_bday.c
+++ taum_and_bday.c
@@ -9,20 +9,13 @@ int main() {
 		ans = 0;
 		scanf("%ld %ld", &b, &w);
 		scanf("%ld %ld %ld", &bc, &wc, &z);
-		if (bc == wc && wc == z) {
+		if (bc >= wc + z) {
+			ans = w*wc + b*(wc + z);
+		}
+		else if (wc >= bc + z) {
+			ans = b*bc + w*(bc + z);
+		} else {
 			ans = b*bc + w*wc;
-		} 
-		else if (bc < z && wc < z) {
-			ans = b * bc + w*wc; 
-		}
-		else if (bc > wc+z) {
-			ans = w*(wc+z) + w*wc;
-		}
-		else if (b == w && wc > bc) {
-			ans = b*bc + w*(bc+z);
-		} 
-		else if (b == w && bc > wc) {
-			ans = w*wc + b*(wc+z);
 		}
 		printf("%ld\n", ans);
 	}
