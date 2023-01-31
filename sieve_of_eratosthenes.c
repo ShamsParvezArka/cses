ab813b12bbc1471d810e09532f2fd5add9849e16
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-31 22:08:22 +0600

    ancient prime number finded

---
+++ sieve_of_eratosthenes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define maxN 30
+
+int main() {
+  int arr[maxN+1];
+	arr[1] = 1;
+  for (int i = 2; i <= maxN; i++) arr[i] = 1;
+
+  for (int i = 2; i <= (int) sqrt(maxN); i++) {
+    for (int j = 2; j <= maxN / i; j++) {
+			arr[i*j] = 0;
+    }
+  }
+
+  for (int i = 0; i < maxN+1; i++) {
+    if (arr[i]) printf("%d ", i);
+  }
+	printf("\n");
+  return 0;
+}
